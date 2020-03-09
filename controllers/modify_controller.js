5e026c3b1d3c1cc0c690eb5705f577b60dfcbf6b
shangweihung <alannn1230@gmail.com>
2020-03-08 17:21:28 -0700

    git setup

---
+++ controllers/modify_controller.js
@@ -0,0 +1,104 @@
+const toRegister = require('../models/register_model');
+const loginAction = require('../models/login_model');
+const Check = require('../service/member_check');
+const encryption = require('../models/encryption');
+const config = require('../config/development_config');
+
+const jwt = require('jsonwebtoken');
+
+check = new Check();
+
+module.exports = class Member {
+    postRegister(req, res, next) {
+        // 進行加密
+        const password = encryption(req.body.password);
+
+        // 獲取client端資料
+        const memberData = {
+            name: req.body.name,
+            email: req.body.email,
+            password: password,
+            create_date: onTime()
+        }
+
+        const checkEmail = check.checkEmail(memberData.email);
+        // 不符合email格式
+        if (checkEmail === false) {
+            res.json({
+                result: {
+                    status: "註冊失敗。",
+                    err: "請輸入正確的Eamil格式。(如1234@email.com)"
+                }
+            })
+        // 若符合email格式
+        } else if (checkEmail === true) {
+            // 將資料寫入資料庫
+            toRegister(memberData).then(result => {
+                // 若寫入成功則回傳
+                res.json({
+                    result: result
+                })
+            }, (err) => {
+                // 若寫入失敗則回傳
+                res.json({
+                    err: err
+                })
+            })
+        }
+    }
+    postLogin(req, res, next) {
+        // 進行加密
+        const password = encryption(req.body.password);
+
+        // 獲取client端資料
+        const memberData = {
+            email: req.body.email,
+            password: password,
+        }
+
+        loginAction(memberData).then(rows => {
+            if (check.checkNull(rows) === true) {
+                res.json({
+                    result: {
+                        status: "登入失敗。",
+                        err: "請輸入正確的帳號或密碼。"
+                    }
+                })
+            } else if (check.checkNull(rows) === false) {
+                // 產生token
+                const token = jwt.sign({
+                    algorithm: 'HS256',
+                    exp: Math.floor(Date.now() / 1000) + (60 * 60), // token一個小時後過期。
+                    data: rows[0].id
+                }, config.secret);
+                res.setHeader('token', token);
+
+                res.json({
+                    result: {
+                        status: "登入成功。",
+                        loginMember: "歡迎 " + rows[0].name + " 的登入！",
+                    }
+                })
+            }
+        })
+    }
+
+
+}
+//取得現在時間，並將格式轉成YYYY-MM-DD HH:MM:SS
+const onTime = () => {
+    const date = new Date();
+    const mm = date.getMonth() + 1;
+    const dd = date.getDate();
+    const hh = date.getHours();
+    const mi = date.getMinutes();
+    const ss = date.getSeconds();
+
+    return [date.getFullYear(), "-" +
+        (mm > 9 ? '' : '0') + mm, "-" +
+        (dd > 9 ? '' : '0') + dd, " " +
+        (hh > 9 ? '' : '0') + hh, ":" +
+        (mi > 9 ? '' : '0') + mi, ":" +
+        (ss > 9 ? '' : '0') + ss
+    ].join('');
+}
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+const MemberModifyMethod = require('../controllers/modify_controller');
+
+memberModifyMethod = new MemberModifyMethod();
+
+router.post('/register', memberModifyMethod.postRegister);
+router.post('/login', memberModifyMethod.postLogin);
+
+module.exports = router;
